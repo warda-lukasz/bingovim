d78d2645bb91b5ad88072c097636d7b471c29dee
Łukasz Warda <lukasz.warda@mokyokit.com>
2025-02-09 18:13:27 +0100

    fix(lsp): correct require paths for default config
    Remove redundant 'lua.' prefix from require paths in LSP configuration
    files. This fixes potential module loading issues and maintains
    consistent require paths across the project.

--- lua/plugins/options/lsp/jsonls.lua
+++ lua/plugins/options/lsp/jsonls.lua
@@ -1,5 +1,5 @@
 local lspconfig = require('lspconfig')
-local default_config = require('lua.plugins.options.lsp.default_config')
+local default_config = require('plugins.options.lsp.default_config')
 
 lspconfig.jsonls.setup(vim.tbl_deep_extend("force", default_config, {
   settings = {
--- lua/plugins/options/lsp/twiggy_language_server.lua
+++ lua/plugins/options/lsp/twiggy_language_server.lua
@@ -1,5 +1,5 @@
 local lspconfig = require('lspconfig')
-local default_config = require('lua.plugins.options.lsp.default_config')
+local default_config = require('plugins.options.lsp.default_config')
 
 lspconfig.twiggy_ls.setup(vim.tbl_deep_extend("force", default_config, {
   filetypes = { "twig" },
