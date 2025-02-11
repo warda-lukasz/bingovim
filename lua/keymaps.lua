4c6eb5702a2c4634ff30baa8df831c5ae89cfa65
Łukasz Warda <lukasz.warda@mokyokit.com>
2025-02-11 14:13:11 +0100

    refactor(keymaps): change diagnostic keymap to <leader>od
    Change diagnostic quickfix list keymap from <leader>q to <leader>od to
    avoid conflict with quit keymap and make it more descriptive (od = open
    diagnostic).

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -26,7 +26,7 @@ map("n", "<leader>hs", "<cmd>split<CR>", { noremap = true, silent = true, desc =
 map('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
 -- Diagnostic keymaps
-map('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+map('n', '<leader>od', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
