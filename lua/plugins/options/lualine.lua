c6f6f5e82d5850b3e1bb7158faae1b918d9077d8
Łukasz Warda <lukasz.warda@mokyokit.com>
2025-04-04 23:21:26 +0200

    feat(theme): change color theme to Catppuccin
    - Add Catppuccin package to plugins/styles.lua
    - Change default theme from carbonfox to catppuccin-mocha
    - Update lualine theme to dracula
    - Update dependencies in lazy-lock.json

--- lua/plugins/options/lualine.lua
+++ lua/plugins/options/lualine.lua
@@ -1,5 +1,5 @@
 require('lualine').setup({
   options = {
-    theme = 'iceberg_dark',
+    theme = 'dracula',
   }
 })
--- lua/plugins/styles.lua
+++ lua/plugins/styles.lua
@@ -8,5 +8,6 @@ return {
       style = "night",
     },
   },
-  { "EdenEast/nightfox.nvim" } -- lazy
+  { "EdenEast/nightfox.nvim" },
+  { "catppuccin/nvim",       name = "catppuccin", priority = 1000 },
 }
