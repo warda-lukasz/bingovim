061489f2f13617e6f5aaa15716ad1f0082139af6
Łukasz Warda <lukasz.warda@mokyokit.com>
2025-05-11 04:03:06 +0200

    refactor(lsp): uproszczenie konfiguracji i usunięcie plików
    Zmieniono konfigurację LSP, przenosząc ustawienia do jednego pliku oraz
    usuwając dedykowane pliki konfiguracyjne dla poszczególnych serwerów.
    Dodano automatyczne włączanie i instalację serwerów przez
    mason-lspconfig.
    Ułatwia to zarządzanie i aktualizację LSP w projekcie.

--- lua/plugins/options/lsp.lua
+++ lua/plugins/options/lsp.lua
@@ -1,43 +1,28 @@
+-- Definiuj globalną zmienną vim lokalnie, aby zapobiec ostrzeżeniom
+local vim = vim
+
+-- Konfiguracja Mason
 require("mason").setup()
 require("mason-lspconfig").setup({
   ensure_installed = {
-    "phpactor",  -- PHP
-    "intelephense",
-    "cssls",     -- CSS
-    "html",      -- HTML
-    "eslint",    -- JavaScript, TypeScript linting
-    "jsonls",    -- JSON
-    "emmet_ls",  -- Emmet for HTML/CSS
-    "yamlls",    -- YAML
-    "bashls",    -- Bash
-    "dockerls",  -- Docker
-    "angularls", -- Angular
-    "lua_ls",    -- Lua
-    "ts_ls",     -- TypeScript
-    "marksman",  -- Markdown
-    "typos_lsp", -- Typos
+    "intelephense",           -- PHP
+    "cssls",                  -- CSS
+    "html",                   -- HTML
+    "eslint",                 -- JavaScript, TypeScript linting
+    "jsonls",                 -- JSON
+    "emmet_ls",               -- Emmet for HTML/CSS
+    "yamlls",                 -- YAML
+    "bashls",                 -- Bash
+    "dockerls",               -- Docker
+    "angularls",              -- Angular
+    "lua_ls",                 -- Lua
+    "ts_ls",                  -- TypeScript
+    "marksman",               -- Markdown
+    "typos_lsp",              -- Typos
     "twiggy_language_server", -- Twig
-    -- "eslint",                -- TypeScript
   },
   automatic_installation = true,
+  automatic_enable = true,
 })
 
--- Load LSPs configurations
-require("plugins.options.lsp.luals")
-require("plugins.options.lsp.dockerls")
-require("plugins.options.lsp.yamlls")
-require("plugins.options.lsp.jsonls")
-require("plugins.options.lsp.html")
-require("plugins.options.lsp.cssls")
-require("plugins.options.lsp.bashls")
-require("plugins.options.lsp.ts_ls")
-require("plugins.options.lsp.marksman")
-require("plugins.options.lsp.typos")
-require("plugins.options.lsp.twiggy_language_server")
-
-
--- PHP Specific:
--- Paid:
 require("plugins.options.lsp.intelephense")
--- Free:
--- require("plugins.options.lsp.phpactor")
--- lua/plugins/options/lsp/twiggy_language_server.lua
+++
@@ -1,6 +0,0 @@
-local lspconfig = require('lspconfig')
-local default_config = require('plugins.options.lsp.default_config')
-
-lspconfig.twiggy_language_server.setup(vim.tbl_deep_extend("force", default_config, {
-  filetypes = { "twig", "html.twig" },
-}))
